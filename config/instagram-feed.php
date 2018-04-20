0c125f42f3a5080ecbf36359f90626317792af13
Michael Joyner <joyner.michael@gmail.com>
2018-04-20 11:46:37 +0800

    improve config file

--- config/instagram-feed.php
+++ config/instagram-feed.php
@@ -1,9 +1,34 @@
 <?php
 
 return [
-    'client_id'               => 'YOUR INSTAGRAM CLIENT ID',
-    'client_secret'           => 'YOUR INSTAGRAM CLIENT SECRET',
+    /*
+     * The client_id from registering your app on Instagram
+     */
+    'client_id'           => 'YOUR INSTAGRAM CLIENT ID',
+
+    /*
+     * The client secret from registering your app on Instagram,
+     * This is not the same as an access token.
+     */
+    'client_secret'       => 'YOUR INSTAGRAM CLIENT SECRET',
+
+    /*
+     * The route that will respond to the Instagram callback during the OAuth process.
+     * Only enter the path without the leading slash. You need to ensure that you have registered
+     * a redirect_uri for your instagram app that is equal to combining the
+     *  app url (from config) and this route
+     */
     'auth_callback_route' => 'instagram/auth/callback',
-    'success_redirect_to'     => 'admin/yay',
-    'failure_redirect_to'     => 'admin/boo'
+
+    /*
+     * On success of the OAuth process you will be redirected to this route.
+     * You may use query strings to carry messages
+     */
+    'success_redirect_to' => 'instagram-auth-success',
+
+    /*
+     * If the OAuth process fails for some reason you will be redirected to this route.
+     * You may use query strings to carry messages
+     */
+    'failure_redirect_to' => 'instagram-auth-failure'
 ];
