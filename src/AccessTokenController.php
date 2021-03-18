0fa613772d01adc0e807f9e082177f85c3def8f1
Yuriy Martini <y.martini@soluzionesoftware.com>
2021-03-18 09:32:45 +0100

    refactoring
    This reverts commit ea18a28d

--- src/AccessTokenController.php
+++ src/AccessTokenController.php
@@ -5,22 +5,23 @@ namespace Dymantic\InstagramFeed;
 
 
 use Exception;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Redirect;
 
 class AccessTokenController
 {
-    public function handleRedirect()
+    public function handleRedirect(Request $request)
     {
         /** @var Profile|null $profile */
-        $profile = Profile::query()->find((int) request('state'));
+        $profile = Profile::query()->find((int) $request->input('state'));
 
         if (!$profile) {
             return Redirect::to(Config::get('instagram-feed.failure_redirect_to'));
         }
 
         try {
-            $profile->requestToken(request());
+            $profile->requestToken($request);
         } catch (Exception $e) {
             return Redirect::to(Config::get('instagram-feed.failure_redirect_to'));
         }
